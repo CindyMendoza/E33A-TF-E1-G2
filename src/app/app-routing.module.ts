818d36494f41e234b50686e9b0990ad3e13e5240
cindy mendoza <mendozaic17@gmail.com>
2022-03-16 22:43:04 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [{
+  path:'home', component: HomeComponent },
+  {path:'aboutus', component: AboutusComponent },
+  {path:'contactus', component: ContactusComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactusComponent,
+    AboutusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
